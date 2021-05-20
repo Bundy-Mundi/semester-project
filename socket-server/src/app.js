039d8d8bbc664a0547709e3ab97f274425ab974a
Bundy-Mundi <bennie.ben095@gmail.com>
2021-05-21 06:03:54 +0900

    Server update

--- socket-server/src/app.js
+++ socket-server/src/app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 import "@babel/polyfill";
 import express from "express";
+import session from "express-session";
+import cors from "cors";
 import http from "http";
 import socketIo from "socket.io";
 import index from "./routes/index";
@@ -11,6 +13,23 @@ import mongoose from "mongoose";
 const port = process.env.PORT || 4001;
 const client_url = process.env.CLIENT_URL || "http://localhost:3000";
 const app = express();
+const SESSION_OPT = {
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true
+  };
+const CORS_OPT = {
+    origin: client_url,
+    credentials: true,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+var sessionMiddleware = session(SESSION_OPT);
+app.use(cors(CORS_OPT));
+app.use(sessionMiddleware);
+app.use((req,res,next) => {
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+})
 app.use(index);
 
 const server = http.createServer(app);
@@ -26,6 +45,9 @@ const io = socketIo(server, SOCKET_OPTS); // < Interesting!
 import "./mongodb";
 
 const chatRoute = io.of('/chat');
+chatRoute.use((socket, next)=>{
+    sessionMiddleware(socket.request, socket.request.res || {}, next);
+})
 chatRoute.on('connection', (socket) => {
     console.log("New client connected");
     socket.on("new connection", async({userID}) => {
