c59544e5eabe6b5cafa9544aa98af83e70cd6cc3
Bundy-Mundi <bennie.ben095@gmail.com>
2021-05-26 23:17:56 +0900

    Add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'fade-in': 'fadein 0.3s ease-in-out',
+        'fade-in-long': 'fadein 0.8s ease-in-out'
+      },
+      keyframes: {
+        fadein: {
+            '0%': { opacity: '0' },
+            '100%': { opacity: '1' },
+        }
+      }
+    },
   },
   variants: {
     extend: {},
