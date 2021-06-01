4ed671af86d9245e74c8b8cb110f971172df758f
Bundy-Mundi <bennie.ben095@gmail.com>
2021-06-01 22:36:24 +0900

    About page disclaimer + DB update

--- src/db/index.js
+++ src/db/index.js
@@ -28,6 +28,21 @@ export const DB = [
         projects:4,
         bio:"",
         current_college: "SMC",
-        img_url:""
+        img_url:"",
+        profile: "Blah",
+        contact:{
+            email: "bennie.ben095@gmail.com",
+            address: "CA Los Angeles",
+            phone: "000-000-0000"
+        },
+        websites:[
+            "https://bundy-mundi.github.io/Today-I-Learned/"
+        ],
+        skills:{
+            javascript: 80,
+            python: 70
+        },
+        experience:"",
+        education: "",
     },
 ]
