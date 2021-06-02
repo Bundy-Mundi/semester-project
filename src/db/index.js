0503591f23e2578d1eb1645b59e713de840d49a7
Bundy-Mundi <bennie.ben095@gmail.com>
2021-06-02 21:15:31 +0900

    Update DB

--- src/db/index.js
+++ src/db/index.js
@@ -6,43 +6,55 @@ export const DB = [
         bio:"I am a developer",
         current_college: "SMC",
         img_url:"/static/img/ben_profile.jpg",
-        profile: "Blah",
+        profile: "",
         contact:{
             email: "bennie.ben095@gmail.com",
             address: "CA Los Angeles",
             phone: "000-000-0000"
         },
         websites:[
-            "https://bundy-mundi.github.io/Today-I-Learned/"
+            "https://bundy-mundi.github.io/Today-I-Learned/",
+            "https://github.com/Bundy-Mundi",
         ],
         skills:{
             javascript: 80,
             python: 70
         },
-        experience:"",
-        education: "",
+        experience:[
+            "Worked as a PHP freelancer to edit Wordpress or Gnuboard based websites.",
+            "Worked as a barista in New Zealand for one year."
+        ],
+        education: [
+            "Santa Monica College",
+            "CSU Long Beach"
+        ],
     },
     { 
         id: 2, 
         name: "Nancy Anatuanya",
         projects:4,
-        bio:"",
+        bio:"Hi I am Nancy",
         current_college: "SMC",
-        img_url:"",
-        profile: "Blah",
+        img_url:"/static/img/nancy_profile.jpg",
+        profile: "",
         contact:{
-            email: "bennie.ben095@gmail.com",
+            email: "anatuanya_nancy_afom@student.smc.edu",
             address: "CA Los Angeles",
             phone: "000-000-0000"
         },
         websites:[
-            "https://bundy-mundi.github.io/Today-I-Learned/"
+            "https://github.com/nancyAfycodes/nancyAfycodes "
         ],
         skills:{
             javascript: 80,
             python: 70
         },
-        experience:"",
-        education: "",
+        experience:[
+            "Assist guest in improving shopping experience via processing items purchased, item returns and exchanges as well as online order pick-up, answering questions and assist other departments as needed",
+            "Provide additional guest assistance through ordering non-in-store items from store warehouse and notifying guest(s) once item is ready for pickup in-store."
+        ],
+        education: [
+
+        ],
     },
 ]
