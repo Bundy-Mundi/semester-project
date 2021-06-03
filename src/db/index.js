5182c0a747c745696bc55d48e03895283263f757
Bundy-Mundi <bennie.ben095@gmail.com>
2021-06-03 18:27:02 +0900

    Add skills in CS + DB updated

--- src/db/index.js
+++ src/db/index.js
@@ -16,10 +16,32 @@ export const DB = [
             "https://bundy-mundi.github.io/Today-I-Learned/",
             "https://github.com/Bundy-Mundi",
         ],
-        skills:{
-            javascript: 80,
-            python: 70
-        },
+        skills:[
+            {
+                name:"JavaScript",
+                score:87
+            },
+            {
+                name:"TypeScript",
+                score:65
+            },
+            {
+                name: "Python",
+                score:70
+            },
+            {
+                name: "Go",
+                score: 50
+            },
+            {
+                name: "Linux",
+                score: 70
+            },
+            {
+                name: "C / C++",
+                score: 65
+            }
+        ],
         experience:[
             "Worked as a PHP freelancer to edit Wordpress or Gnuboard based websites.",
             "Worked as a barista in New Zealand for one year."
@@ -33,10 +55,10 @@ export const DB = [
         id: 2, 
         name: "Nancy Anatuanya",
         projects:4,
-        bio:"Hi I am Nancy",
+        bio: "Hi I am Nancy",
         current_college: "SMC",
         img_url:"/static/img/nancy_profile.jpg",
-        profile: "",
+        profile: "I am a Computer Science Student, interested in Data Science, and Web design",
         contact:{
             email: "anatuanya_nancy_afom@student.smc.edu",
             address: "CA Los Angeles",
@@ -45,16 +67,22 @@ export const DB = [
         websites:[
             "https://github.com/nancyAfycodes/nancyAfycodes "
         ],
-        skills:{
-            javascript: 80,
-            python: 70
-        },
+        skills:[
+            {
+                name:"JavaScript",
+                score:80
+            },
+            {
+                name: "Python",
+                score:65
+            }
+        ],
         experience:[
             "Assist guest in improving shopping experience via processing items purchased, item returns and exchanges as well as online order pick-up, answering questions and assist other departments as needed",
             "Provide additional guest assistance through ordering non-in-store items from store warehouse and notifying guest(s) once item is ready for pickup in-store."
         ],
         education: [
-
+            "Santa Monica College"
         ],
     },
 ]
